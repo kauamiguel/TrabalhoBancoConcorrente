226606618152d9b6c048fa20a6814838f8031454
kauamiguel <kauamcm@gmail.com>
2024-05-05 14:29:33 -0300

    Subindo o esqueleto do projeto

---
+++ Funcionarios/Camareira.java
@@ -0,0 +1,6 @@
+package Funcionarios;
+
+public class Camareira{
+    
+}
+
---
+++ Pessoas/Pessoa.java
@@ -0,0 +1,13 @@
+package Pessoas;
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private String cpf;
+
+    public Pessoa(String nome, int idade, String cpf) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+    }
+
+}
